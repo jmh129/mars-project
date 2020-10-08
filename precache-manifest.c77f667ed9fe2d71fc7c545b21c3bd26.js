72bcff26862becaa87b4a86f2c39bf2c798f5752
James Holding <james.holding90@gmail.com>
2020-10-07 21:27:28 -0400

    Updates

---
+++ precache-manifest.c77f667ed9fe2d71fc7c545b21c3bd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e2389701ad0731973e728c88e17312",
+    "url": "/mars-project/index.html"
+  },
+  {
+    "revision": "b0286c8306325fbcc732",
+    "url": "/mars-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "480e344d996c82a89859",
+    "url": "/mars-project/static/js/2.b0f7aa01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mars-project/static/js/2.b0f7aa01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0286c8306325fbcc732",
+    "url": "/mars-project/static/js/main.ccdd69ac.chunk.js"
+  },
+  {
+    "revision": "3ee56f2e611c25b54f23",
+    "url": "/mars-project/static/js/runtime-main.925b594e.js"
+  }
+]);
